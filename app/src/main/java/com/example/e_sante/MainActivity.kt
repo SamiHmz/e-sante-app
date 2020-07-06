053f63b11d16ab40e1923d6dfaa5b34f165d07b1
dahamni_youcef <gy_dahamni@esi.dz>
2020-07-06 23:06:06 +0100

    mod 06/07/2020

--- app/src/main/java/com/example/e_sante/MainActivity.kt
+++ app/src/main/java/com/example/e_sante/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_demande_consultation)
+        setContentView(R.layout.fragment_login_signup)
     }
 }
