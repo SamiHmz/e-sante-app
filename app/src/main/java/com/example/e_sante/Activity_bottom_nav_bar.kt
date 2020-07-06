82d387d0dacd7b323eae3b48fb4258a0840cb2de
dahamni_youcef <gy_dahamni@esi.dz>
2020-07-06 23:05:14 +0100

    mod 06/07/2020

---
+++ app/src/main/java/com/example/e_sante/Activity_bottom_nav_bar.kt
@@ -0,0 +1,12 @@
+package com.example.e_sante
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_bottom_nav_bar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_nav_bar)
+    }
+}
