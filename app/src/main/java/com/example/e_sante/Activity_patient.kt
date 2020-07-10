b529091c9565176b76ef643886f55b7efc3f8ffa
dahamni_youcef <gy_dahamni@esi.dz>
2020-07-10 13:23:40 +0100

    10/07/2020 13.23 rename id

--- app/src/main/java/com/example/e_sante/Activity_patient.kt
+++ app/src/main/java/com/example/e_sante/Activity_patient.kt
@@ -3,10 +3,10 @@ package com.example.e_sante
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Activity_bottom_nav_bar : AppCompatActivity() {
+class Activity_patient : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bottom_nav_bar)
+        setContentView(R.layout.activity_patient)
     }
 }
