cc69f4f5563078720f55536892656ce4bdc24d68
SamiHmz <samihamaizi36@gmail.com>
2020-07-16 20:56:35 +0100

    login,sing up ,landing page layout edited

--- app/src/main/java/com/example/e_sante/RetrofitService.kt
+++ app/src/main/java/com/example/e_sante/RetrofitService.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitService {
     val endpoint: Endpoint by lazy {
-        Retrofit.Builder().baseUrl("http://3a8ce159acca.ngrok.io").
+        Retrofit.Builder().baseUrl("http://9c3ad32ee0f5.ngrok.io").
         addConverterFactory(GsonConverterFactory.create()).
         build().create(Endpoint::class.java)
     }
